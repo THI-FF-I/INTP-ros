a083a7e9e51f43d71ff37bfb9b261c0cfce05074
J0las <52259564+J0las@users.noreply.github.com>
2022-11-06 15:01:24 +0100

    Make getter and setter constexpr

--- jps_maze/src/jps_maze/lib/jps_maze_game/include/jps_maze_game/player.hpp
+++ jps_maze/src/jps_maze/lib/jps_maze_game/include/jps_maze_game/player.hpp
@@ -26,28 +26,28 @@ namespace jps_maze_game
     public:
         Player(const player_id_t player_id, const team_t team_id, const std::string &player_name);
 
-        void set_x(const coord_t new_x)
+        constexpr void set_x(const coord_t new_x)
         {
             x = new_x;
         }
-        void set_y(const coord_t new_y)
+        constexpr void set_y(const coord_t new_y)
         {
             y = new_y;
         }
-        void set_has_flag(bool new_flag)
+        constexpr void set_has_flag(bool new_flag)
         {
             has_flag = new_flag;
         }
 
-        coord_t get_x() const
+        constexpr coord_t get_x() const
         {
             return x;
         }
-        coord_t get_y() const
+        constexpr coord_t get_y() const
         {
             return y;
         }
-        team_t get_team() const
+        constexpr team_t get_team() const
         {
             return team;
         }
