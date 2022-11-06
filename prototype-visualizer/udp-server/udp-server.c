32338f6c48d31021c13432335a8ecaf5e038d703
Simon Demuth <sidemuth705@googlemail.com>
2022-11-06 15:27:15 +0100

    Added docker dev env + dummy udp server, sending area array

---
+++ prototype-visualizer/udp-server/udp-server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#define ARENA_SIZE 64
+
+int main()
+{
+    //initialize and fill dummy arena
+    int arena[ARENA_SIZE][ARENA_SIZE];
+
+    int k = 0;
+
+    for(int i = 0; i < ARENA_SIZE; i++)
+    {
+
+        for(int n = 0; n < ARENA_SIZE; n++)
+        {
+
+            arena[i][n] = k;
+            k++;
+            if(k>4)k=0;
+
+        }
+
+    }
+
+    // create a udp socket
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // address for socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET; 
+    server_address.sin_port = htons(42069);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind address to socket
+    //TODO: make some error handling, but screw that, I am a consultant 
+    bind(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    //smash it
+    while(true)
+    {
+        sendto(network_socket, arena, sizeof(arena), 0, (const struct sockaddr *) &server_address, sizeof(server_address));
+        printf("Sended!\n");
+    }
+
+    return 0;
+
+}
